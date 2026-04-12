bcb0ca349b6cff5fed0b8f752ac5f6c84cff0296
tm1haha <85028077+Ming13k6@users.noreply.github.com>
2026-04-12 23:50:07 +0700

    Update audio.py

--- src/audio.py
+++ src/audio.py
@@ -1,1 +1,10 @@
+import soundfile as sf
+import numpy as np
 
+def load_audio(path):
+    data, sr = sf.read(path)
+
+    if len(data.shape) == 1:
+        data = data.reshape(-1, 1)
+
+    return data, sr
